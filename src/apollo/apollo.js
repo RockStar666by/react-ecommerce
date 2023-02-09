b3c2500983cdf262f6b1974d8a88e7781c92c9d9
Artiom Savchuk <52502654+RockStar666by@users.noreply.github.com>
2023-02-09 21:13:36 +0300

    Update apollo.js

--- src/apollo/apollo.js
+++ src/apollo/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://junior-react-endpoint-production.up.railway.app/',
+  uri: 'https://junior-react-endpoint-omega.vercel.app/',
   cache: new InMemoryCache()
 });
